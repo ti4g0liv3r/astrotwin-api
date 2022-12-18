173d4187c4fb8fa68511c8ae716fffbc5b1c89dd
Tiago Pinheiro <tiagoliver@pm.me>
2022-12-18 01:50:47 +0100

    Added comment with pending features

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+///////////////////////////////////////////////////////
+// -*-*--*-*--*-*- SUGGESTED FEATURES -*-*--*-*--*-*-
+//
+// * SUGGESTED FEATURES
+//
+// - ADD "ANNUAL PROFECTIONS" BASED ON ASCENDANT (FR) => BIRTHCHART
+// - ADD "INTERESTS" (COMMUNITY LIKE) SECTION (YOGA, TAROT, WICCAN, SPIRITUALITY)
+// - ADD POSSIBILITY TO USERS INTERACT THROUGH MESSAGES (SOCKET.IO) INTEGRATION
+// - ADD TAROT BIRTH CARDS =>
+//   https://tarotismyreligion.tumblr.com/post/88536899559/photoset_iframe/tarotismyreligion/tumblr_n716pim9DI1smm2xn/500/false
+// - ADD POSSIBILITY TO USERS INTERACT ON POSTS (LIKE, COMMENT, SHARE)
+// - ADD JOURNALING SECTION
+// - ADD WIKI PAGE WITH (OLIS, CANDLES, GEMSTONES, INCESES, ETC)
+// - ADD USER'S KARMA (BASED ON USER'S ACTIVITY) => ORKUT INSPIRED
+//
+//
+// -*-*--*-*--*-*-  MUST HAVE  -*-*--*-*--*-*-
+//
+// - ADD A SCRIPT TO DELETE ALL USER'S INFORMATION ONCE ACCOUNT DELETED
+//
+///////////////////////////////////////////////////////
+
 const express = require("express");
 const mongoose = require("mongoose");
 
